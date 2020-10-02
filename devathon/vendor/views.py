f55e19a29358682e4178ec98955aa6793174b9fb
Abhishek Pratap Singh <navodayanabhishek@gmail.com>
2020-10-02 14:08:26 +0530

    small fix in vendor login

--- devathon/vendor/views.py
+++ devathon/vendor/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 # Create your views here.
 from django.contrib.auth import authenticate, login
 
 
 def dashboard(request):
-    return render(request, 'vendor/dashboard.html')
-
+    if request.user.is_authenticated:
+        return render(request, 'vendor/dashboard.html')
+    return redirect("/accounts/vendor/")
 
 def order(request):
-    return render(request, 'vendor/order.html')
+    if request.user.is_authenticated:
+        return render(request, 'vendor/order.html')
+    return redirect("/accounts/vendor/")
